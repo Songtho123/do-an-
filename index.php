5db026a1eaa1cdb4414120a98d974d8f211b2b48
Nguyen Huy <huy246302@gmail.com>
2023-12-03 23:01:53 +0700

    more

--- index.php
+++ index.php
@@ -75,6 +75,17 @@ while ($row=mysqli_fetch_array($query)) {
         </div>
 
       <?php include('includes/sidebar.php');?>
+      <style>
+        /* CSS for the card header */
+        .sticky-300 {
+            position: sticky;
+            top: 300px;
+        }
+        .sticky-700 {
+            position: sticky;
+            top: 700px;
+        }
+      </style>
       </div>
     </div>
     <script src="vendor/jquery/jquery.min.js"></script>
