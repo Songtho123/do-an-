0fb9eb6efa3b1a2179d0c1cc1a141d237c7ca009
Nguyen Huy <huy246302@gmail.com>
2023-12-14 23:20:06 +0700

    ctv-login

--- admin/login-author.php
+++ admin/login-author.php
@@ -1,78 +1,128 @@
+<?php
+ session_start();
+
+include('includes/config.php');
+
+if(isset($_POST['login']))
+  {
+     $uname=$_POST['username'];
+    $password=$_POST['password'];
+$sql =mysqli_query($con,"SELECT AdminUserName,AdminEmailId,AdminPassword FROM tbladmin WHERE (AdminUserName='$uname' || AdminEmailId='$uname')");
+ $num=mysqli_fetch_array($sql);
+if($num>0)
+{
+$hashpassword=$num['AdminPassword']; 
+if (password_verify($password, $hashpassword)) {
+$_SESSION['login']=$_POST['username'];
+    echo "<script type='text/javascript'> document.location = 'dashboard.php'; </script>";
+  } else {
+echo "<script>alert('THỬ LẠI BẠN EIIIII!!!!');</script>";
+ 
+  }
+}
+else{
+echo "<script>alert('Tài khoản chưa đăng ký');</script>";
+  }
+ 
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Đăng nhập cộng tác viên</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            background-color: #f4f4f4;
-            margin: 0;
-            padding: 0;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            height: 100vh;
-        }
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Cổng tin tức.">
+        <meta name="author" content="PHPGurukul">
+        <title>Cổng tin tức | CTV Panel</title>
 
-        .container {
-            background-color: #fff;
-            padding: 20px;
-            border-radius: 15px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-            width: 300px;
-            text-align: center;
-            border: 2px solid #007bff; /* Highlighted border color */
-        }
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/core.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/components.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/icons.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/pages.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/menu.css" rel="stylesheet" type="text/css" />
+        <link href="assets/css/responsive.css" rel="stylesheet" type="text/css" />
 
-        form {
-            display: flex;
-            flex-direction: column;
-        }
+        <script src="assets/js/modernizr.min.js"></script>
 
-        label {
-            margin-top: 10px;
-        }
+    </head>
 
-        input {
-            margin-bottom: 10px;
-            padding: 8px;
-            border: 1px solid #ccc;
-            border-radius: 4px;
-        }
 
-        button {
-            background-color: #007bff;
-            color: #fff;
-            padding: 10px;
-            border: none;
-            border-radius: 4px;
-            cursor: pointer;
-        }
+    <body class="bg-transparent">
 
-        p {
-            margin-top: 15px;
-        }
+        <section>
+            <div class="container-alt">
+                <div class="row">
+                    <div class="col-sm-12">
 
-        a {
-            color: #007bff;
-            text-decoration: none;
-        }
-    </style>
-</head>
-<body>
-    <div class="container">
-        <!-- Login Form -->
-        <form action="login_process.php" method="post">
-            <h2>Đăng nhập</h2>
-            <label for="login_username">Tên:</label>
-            <input type="text" name="login_username" required>
-            <label for="login_password">Mật khẩu:</label>
-            <input type="password" name="login_password" required>
-            <button type="submit">Đăng nhập</button>
-        </form>
-        <p>Chưa có tài khoản<a href="/admin/register-author.php">Đăng ký công tác viên</a>.</p>
-    </div>
-</body>
+                        <div class="wrapper-page">
+
+                            <div class="m-t-40 account-pages">
+                                <div class="text-center account-logo-box">
+                                    <h2 class="text-uppercase">
+                                        <a href="index.html" class="text-success">
+                                            <a style="color: #ffffff;">
+                                                News Portal
+                                                <br/>
+                                                CTV login
+                                            </a>
+                                        </a>
+                                    </h2>
+                                </div>
+                                <div class="account-content">
+                                    <form class="form-horizontal" method="post">
+
+                                        <div class="form-group ">
+                                            <div class="col-xs-12">
+                                                <input class="form-control" type="text" required="" name="username" placeholder="Username or email" autocomplete="off">
+                                            </div>
+                                        </div>
+
+                                        <div class="form-group">
+                                            <div class="col-xs-12">
+                                                <input class="form-control" type="password" name="password" required="" placeholder="Password" autocomplete="off">
+                                            </div>
+                                        </div>
+
+
+                     
+                                        <div class="form-group account-btn text-center m-t-10">
+                                            <div class="col-xs-12">
+                                                <button class="btn w-md btn-bordered btn-danger waves-effect waves-light" type="submit" name="login">Log In</button>
+                                            </div>
+                                        </div>
+
+                                    </form>
+
+                                    <div class="clearfix"></div>
+
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+          </section>
+
+        <script>
+            var resizefunc = [];
+        </script>
+
+        <!-- jQuery  -->
+        <script src="assets/js/jquery.min.js"></script>
+        <script src="assets/js/bootstrap.min.js"></script>
+        <script src="assets/js/detect.js"></script>
+        <script src="assets/js/fastclick.js"></script>
+        <script src="assets/js/jquery.blockUI.js"></script>
+        <script src="assets/js/waves.js"></script>
+        <script src="assets/js/jquery.slimscroll.js"></script>
+        <script src="assets/js/jquery.scrollTo.min.js"></script>
+
+        <!-- App js -->
+        <script src="assets/js/jquery.core.js"></script>
+        <script src="assets/js/jquery.app.js"></script>
+
+    </body>
 </html>
